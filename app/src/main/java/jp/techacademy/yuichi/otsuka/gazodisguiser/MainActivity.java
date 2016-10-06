5ba05c746e416736d0e17760f6e49912c1251407
OtsukaYuichi <yui2056@yahoo.co.jp>
2016-10-07 01:38:29 +0900

    layout modified 1

--- app/src/main/java/jp/techacademy/yuichi/otsuka/gazodisguiser/MainActivity.java
+++ app/src/main/java/jp/techacademy/yuichi/otsuka/gazodisguiser/MainActivity.java
@@ -2,12 +2,22 @@ package jp.techacademy.yuichi.otsuka.gazodisguiser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonFS = (Button) findViewById(R.id.fileSelectorButton);
+        buttonFS.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
